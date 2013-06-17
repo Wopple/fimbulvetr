5eca30ba7e64b02ab1ef1fd40ad23bc821a771b9
southpawhare@gmail.com <southpawhare@gmail.com@880f2e3c-e94e-cd04-d2e1-04bf329775c3>
2013-06-17 06:04:42 +0000

    Adjusted visuals for platforms
    git-svn-id: http://fimbulvetr.googlecode.com/svn/trunk@307 880f2e3c-e94e-cd04-d2e1-04bf329775c3

--- platform.py
+++ platform.py
@@ -10,7 +10,13 @@ class Platform(object):
         self.rect = pygame.Rect( inPos, (inLength, PLATFORM_HEIGHT) )
 
         self.image = pygame.Surface( (self.rect.width, self.rect.height) )
-        self.image.fill(PLATFORM_COLORS[inTerrain])
+        
+        self.image.fill(BLACK)
+        
+        innerImage = pygame.Surface( (self.rect.width, self.rect.height - (PLATFORM_BORDER_SIZE * 2)) )
+        innerImage.fill(PLATFORM_COLORS[inTerrain])
+        
+        self.image.blit(innerImage, (0, PLATFORM_BORDER_SIZE))
 
     def draw(self, screen, inOffset):
         screen.blit(self.image, add_points(self.rect.topleft, inOffset))
