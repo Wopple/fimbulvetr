1c2e3250ab23d2a3a0514236458f754c0c8a0f01
Daniel Tashjian <thewopple@gmail.com>
2013-11-14 21:32:48 -0500

    Added a frame rate limiter.

---
+++ util/framerate.py
@@ -0,0 +1,38 @@
+import time
+
+class FrameRate(object):
+    """ Simple frame rate limiter """
+
+    def __init__(self, fps=60):
+        self.fps = fps
+        self.prev = time.time()
+
+    # public
+
+    def next(self):
+        self.prev = self.endFrame(self.prev)
+
+    # private
+
+    def endFrame(self, prev):
+        while True:
+            next = time.time()
+            diff = (1.0 / self.fps) - (next - prev)
+
+            if diff <= 0:
+                break
+
+            if diff > 0.0015:
+                time.sleep(0.001)
+            else:
+                time.sleep(0)
+
+        return next
+
+if __name__ == "__main__":
+    fps = 60
+    rate = FrameRate(fps)
+
+    for i in range(120):
+        print "ping", (i % fps)
+        rate.next()
