327ab032122449521be72de963ca6bed3b905a5b
Daniel Tashjian <thewopple@gmail.com>
2013-12-23 23:25:40 -0500

    Improved the process utility.

--- src/common/util/process.py
+++ src/common/util/process.py
@@ -1,7 +1,13 @@
 from multiprocessing import Process, Queue
 
+# PUBLIC
+
 def process(fn, state=None):
     """
+    Runs a function on a new process. The return value is passed to the queue
+    returned by this function. An optional argument can be provided to the
+    function.
+
     fn: function to call, 1 arg if state is not None, 0 args otherwise
     state: optional state to pass to the function
     return: the queue which will be provided the return value of the function
@@ -11,6 +17,21 @@ def process(fn, state=None):
     Process(target=process_fn, args=(fn, queue, state)).start()
     return queue
 
+def get_last(queue):
+    """
+    queue: queue to empty
+    return: the last item in the queue
+    """
+
+    ret = None
+
+    while not queue.empty():
+        ret = queue.get()
+
+    return ret
+
+# PRIVATE
+
 def process_fn(fn, queue, state):
     if state is not None:
         ret = fn(state)
@@ -18,11 +39,3 @@ def process_fn(fn, queue, state):
         ret = fn()
 
     queue.put(ret)
-
-def get_latest(queue):
-    ret = None
-
-    while not queue.empty():
-        ret = queue.get()
-
-    return ret
