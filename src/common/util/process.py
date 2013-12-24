50fb1892f2c5d756e59b5c0e722bcaaf5752c895
Daniel Tashjian <thewopple@gmail.com>
2013-12-23 19:26:12 -0500

    Added simple tool for multi-processing.

---
+++ src/common/util/process.py
@@ -0,0 +1,20 @@
+from multiprocessing import Process, Queue
+
+def process(fn, state=None):
+    """
+    fn: function to call, 1 arg if state is not None, 0 args otherwise
+    state: optional state to pass to the function
+    return: the queue which will be provided the return value of the function
+    """
+
+    queue = Queue(1)
+    Process(target=process_fn, args=(fn, queue, state)).start()
+    return queue
+
+def process_fn(fn, queue, state):
+    if state is not None:
+        ret = fn(state)
+    else:
+        ret = fn()
+
+    queue.put(ret)
