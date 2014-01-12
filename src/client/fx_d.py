d0a5f8751b597caf0647b847010d6b36e1280287
Daniel Tashjian <thewopple@gmail.com>
2014-01-12 16:00:14 -0500

    Fixed some bugs with fx_d.

--- src/client/fx_d.py
+++ src/client/fx_d.py
@@ -9,20 +9,20 @@ from client.textrect import render_textrect
 
 class FXDrawable(ItemDrawable):
     def __init__(self, item):
-        super(FXDrawable, self).__init__(item, rect=item.rect)
+        super(FXDrawable, self).__init__(item)
 
     def draw(self, screen, camera=None):
         frame = self.item.frames[self.item.frameNum]
         imageData = FX_IMAGES[frame[fx.IMAGE_DATA]]
         image = imageData[IMAGE]
         offset = imageData[OFFSET]
-        width = image.get_size()[0]
+        size = image.get_size()
 
         if not self.item.facingRight:
             image = pygame.transform.flip(image, True, False)
-            offset = (-offset[0] + width, offset[1])
+            offset = (-offset[0] + size[0], offset[1])
 
-        self.rect = Rect(self.getPos(offset), width)
+        self.rect = Rect(self.getPos(offset), size)
         self.image = image
         super(FXDrawable, self).draw(screen, camera)
 
